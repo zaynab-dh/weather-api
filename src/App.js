8d2867bd095a258da6a9769ff7f7abe3c374eff0
Zaynab Dhaybi <zaynab.dhaybi@gmail.com>
2021-03-25 21:10:49 +0200

    Weather API: Getting the LIVE weather data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+
+import Api from "./components/Search";
 import Currentweather from "./components/Currentweather"
 import Weathernext from "./components/Weathernext"
 import FakeWeather from "./data/FakeWeather.json";
@@ -9,6 +10,8 @@ import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
+
+
 
 class App extends Component {
   constructor(props) {
@@ -28,18 +31,10 @@ class App extends Component {
         {/* <SayHi />
         <SayHello color="black" name={this.state.name} />
          */}
-         <Search handleInput={this.handleInputChange} />
+         
          <div className="main">
-          <Currentweather/>
-          <div className="weather-next24h">
-            <Weathernext time={FakeWeather.list[0].dt_txt} icon={FakeWeather.list[0].weather[0].main} temp={FakeWeather.list[0].main.temp}/>
-            <Weathernext time={FakeWeather.list[1].dt_txt} icon={FakeWeather.list[1].weather[0].main} temp={FakeWeather.list[1].main.temp}/>
-            <Weathernext time={FakeWeather.list[2].dt_txt} icon={FakeWeather.list[2].weather[0].main} temp={FakeWeather.list[2].main.temp}/>
-            <Weathernext time={FakeWeather.list[3].dt_txt} icon={FakeWeather.list[3].weather[0].main} temp={FakeWeather.list[3].main.temp}/>
-            <Weathernext time={FakeWeather.list[4].dt_txt} icon={FakeWeather.list[4].weather[0].main} temp={FakeWeather.list[4].main.temp}/>
-            <Weathernext time={FakeWeather.list[5].dt_txt} icon={FakeWeather.list[5].weather[0].main} temp={FakeWeather.list[5].main.temp}/>
-            <Weathernext time={FakeWeather.list[6].dt_txt} icon={FakeWeather.list[6].weather[0].main} temp={FakeWeather.list[6].main.temp}/>
-          </div>
+          <Api handleInput={this.handleInputChange} />
+          
          </div>
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,54 @@
 import React from "react";
 import Button from "react-bootstrap/Button"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import fog from "../img/weather-icons/fog.svg";
+import rain from "../img/weather-icons/rain.svg";
+import snow from "../img/weather-icons/snow.svg";
+import storm from "../img/weather-icons/storm.svg";
 import clear from "../img/weather-icons/clear.svg";
+import clouds from "../img/weather-icons/clouds.svg";
+import unknown from "../img/weather-icons/unknown.svg";
+import drizzle from "../img/weather-icons/drizzle.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../img/weather-icons/partlycloudy.svg";
 import "./Search.css";
-class Search extends React.Component {
-  state = {
-    input: ""
-  };
+import { useState } from 'react';
+const ap = {
+  key: "5b32436ff4230918f547069b968b2d0b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+function img (temp){
+  let a=Math.round(temp)
+  console.log(temp);
+  if(a<300){return require("../img/weather-icons/storm.svg")}
+  if(a>300 && a<499){return require("../img/weather-icons/drizzle.svg")}
+  if(a>500 && a<599){return require("../img/weather-icons/rain.svg")}
+  if(a>600 && a<699){return require("../img/weather-icons/snow.svg")}
+  if(a>700 && a<799){return require("../img/weather-icons/fog.svg")}
+  if(a=800){return require("../img/weather-icons/clear.svg")}
+  if(a=801){return require("../img/weather-icons/partlycloudy.svg")}
+  if(a>801 && a<805){return require("../img/weather-icons/mostlycloudy.svg")}
+}
+function Api() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-  render() {
+  const find = evt => {
+    if (evt.key === "Enter") {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query},CA&appid=5b32436ff4230918f547069b968b2d0b`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result)
+      });
+    }
+  }
+
+
+  
+
+ 
     return (
       <div className="search">
       
@@ -16,9 +56,12 @@ class Search extends React.Component {
         <input 
           type="text"
           id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
-          }}
+          onChange={e => setQuery(e.target.value)
+          }
+          value={query}
+          onKeyPress = {find}
+            
+          
         />
         <Button variant="info"
           onClick={event => {
@@ -27,8 +70,31 @@ class Search extends React.Component {
         >
           FIND WEATHER
         </Button>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+          <div className="weather-row">
+          <div className= "weather-icon"><img src={img(weather.weather[0].id)} alt="partlycloudy icon" /></div> 
+            <div className="weather-description">overcast clouds</div>
+            <div className="temperature">
+              <div className="temp-text">Temperature</div>
+              <div className="temp-value">{parseInt((weather.main.temp)-273.15)}°c to {Math.ceil((weather.main.temp)-273.15)}°C</div>
+            </div>
+            <div className="weather-row-ph">
+              <div className="humidity">
+                <div className="humidity-text">Humidity</div>
+                <div className="humidity-value">{Math.round(weather.main.humidity)}%</div>
+              </div>
+              <div className="pressure">
+                <div className="pressure-text">Pressure</div>
+                <div className="pressure-value">{Math.round(weather.main.pressure)}</div>
+              </div>
+            </div>
+          </div>
+         
+            </div>
+            ) : ('')}
       </div>
     );
   }
-}
-export default Search;
+
+export default Api;
